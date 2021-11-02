5f17c32c19495906b7bc812103cae4131b917406
GSNeves <79671592+GSNeves@users.noreply.github.com>
2021-11-02 17:51:37 -0300

    .
    .

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <string>
-#include <list>
-#include <vector>
 #include "parser.hpp"
 
 using namespace aria::csv;
@@ -11,7 +9,7 @@ const int ALPHABET_SIZE = 26;
 
 struct TrieNode
 {
-    struct TrieNode *children[ALPHABET_SIZE]; 
+    struct TrieNode *children[ALPHABET_SIZE];
     // sofifa_id é zero se não for fim de nome, se for diferente de zero, representa
     // o fim do nome e o id sofifa do jogador
     int sofifa_id;
@@ -24,48 +22,39 @@ struct tipoNo{
     int total;
 };
 
-typedef struct rating UserRating; 
+typedef struct rating UserRating;
 struct rating{
   int user_id;
   int sofifa_id;
   float rating;
 };
 
-typedef struct player Player; 
+typedef struct player Player;
 struct player{
   int sofifa_id;
   string name;
   string position;
-  int reviewCout;
-  float reviewTotal;
 };
 
-bool playerReviewCount(vector<list<player>> &table,int player_id,float review)
+unsigned long djb2Hash(string str)
 {
-    int index = player_id % table.size();
-    list<player>::iterator it = table.at(index).begin();
-    while (it != table.at(index).end())
-    {
-        if ((*it).sofifa_id == player_id)
-        {
-          (*it).reviewCout++;
-          (*it).reviewTotal += review;
-          return true;
-        }
-        advance(it,1);
-    }
-    return false;
+    unsigned long hash = 5381;
+    for (auto c : str)
+        hash = ((hash << 5) + hash + c);
+
+    return hash;
 }
+
 // Returns new trie node (initialized to NULLs)
 struct TrieNode *getNode(void)
 {
     struct TrieNode *pNode =  new TrieNode;
- 
+
     pNode->sofifa_id = 0;
- 
+
     for (int i = 0; i < ALPHABET_SIZE; i++)
         pNode->children[i] = NULL;
- 
+
     return pNode;
 }
 
@@ -75,92 +64,69 @@ struct TrieNode *getNode(void)
 void insert(struct TrieNode *root, string key, int sofifa_id)
 {
     struct TrieNode *pCrawl = root;
- 
+
     for (int i = 0; i < key.length(); i++)
     {
         int index = key[i] - 'a';
         if (!pCrawl->children[index])
             pCrawl->children[index] = getNode();
- 
+
         pCrawl = pCrawl->children[index];
     }
- 
+
     // mark last node as leaf
     pCrawl->sofifa_id = sofifa_id;
 }
 
 // Returns true if key presents in trie, else
 // false
-bool search(struct TrieNode *root, string key)
+int search(struct TrieNode *root, string key)
 {
     struct TrieNode *pCrawl = root;
- 
+
     for (int i = 0; i < key.length(); i++)
     {
         int index = key[i] - 'a';
         if (!pCrawl->children[index])
-            return false;
- 
+            return 0;
+
         pCrawl = pCrawl->children[index];
     }
- 
-    return (pCrawl != NULL && (pCrawl->sofifa_id != 0));
-}
-void printTable(vector<list<Player>> &table)
-{
-    for (int i = 0; i < (int)table.size(); i++)
-    {
-        cout << i << ": ";
-        list<Player>::iterator it = table.at(i).begin();
-        while (it != table.at(i).end())
-        {
-            cout << (*it).sofifa_id << " "<<(*it).name << " " << (*it).position << " " <<(*it).reviewCout << " " <<(*it).reviewTotal << "->";
-            advance(it,1);
-        }
-        cout << "\n";
+
+    if(pCrawl != NULL && (pCrawl->sofifa_id != 0)){
+        return pCrawl->sofifa_id;
+    }
+    else{
+        return 0;
     }
 }
+
 void le_entrada(){
 
 
 }
 
 int main() {
-  ifstream f("rating.csv");
-  CsvParser parser(f);
-  vector<UserRating> ratings;
-  ifstream f2("players_clean2.csv");
-  CsvParser parser2(f2);
-  vector<list<player>> tablePlayer(5000);
+    struct TrieNode *root = getNode();
 
+    ifstream f2("players_clean2.csv");
+  CsvParser parser2(f2);
+  vector<Player> players;
   for (auto& row : parser2) {
     try{
     Player player;
     player.sofifa_id=stoi(row.at(0));
     player.name=row.at(1);
     player.position=row.at(2);
-    player.reviewCout=0;
-    player.reviewTotal=0;
-    int hash = player.sofifa_id % tablePlayer.size();
-    tablePlayer.at(hash).push_back(player);
+    players.push_back(player);
       }catch(const invalid_argument e){
-          cout << "pula primeira linha";
+          cout << "pula primeira linha\n";
       }
   }
-    vector<list<UserRating>> tableReviews(30000);
-
-  for (auto& row : parser) {
-    try{
-    UserRating rating;
-    rating.user_id=stoi(row.at(0));
-    rating.sofifa_id=stoi(row.at(1));
-    rating.rating=stof(row.at(2));
-    int hash= rating.user_id % tableReviews.size();
-    tableReviews.at(hash).push_back(rating);
-    playerReviewCount(tablePlayer,rating.sofifa_id,rating.rating);
-    }
-    catch(const invalid_argument e){
-        cout << "pula primeira linha";
-    }
+  for(Player player:players){
+    insert(root, player.name, player.sofifa_id);
   }
+  //int achou = search(root, "Thiago Emiliano da Silva");
+
+  cout << "a" << "\n";
 }
